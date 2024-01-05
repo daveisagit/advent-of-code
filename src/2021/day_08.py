702d86eec4127a68de979e3a114b088e400b34b3
Dave Budd <david.budd@ovo.com>
2024-01-05 11:00:25 +0000

    d8

--- src/2021/day_08.py
+++ src/2021/day_08.py
@@ -3,6 +3,7 @@
 """
 
 from collections import defaultdict
+from itertools import permutations
 from common.aoc import file_to_list, aoc_part, get_filename
 from common.general import tok
 
@@ -52,8 +53,50 @@ def solve_part_a(data) -> int:
     return len(all_unique_digits)
 
 
-def resolve_signal_map(signals) -> dict:
+def resolve_signal_map_neat(signals) -> dict:
     """Given a set of signals output a mapping to the defaults"""
+
+    def map_valid(m):
+        """Return True if the map is valid"""
+        for signal in signals:
+            if len(signal) in unique_lengths:
+                continue
+            act_signal = "".join(sorted([m[c] for c in signal]))
+            if act_signal not in digit_to_segments.values():
+                return False
+        return True
+
+    signal_1 = set([signal for signal in signals if len(signal) == 2][0])
+    signal_7 = set([signal for signal in signals if len(signal) == 3][0])
+    signal_4 = set([signal for signal in signals if len(signal) == 4][0])
+
+    restrictions = []
+    restrictions.append(signal_7 - signal_1)  # a
+    restrictions.append(signal_4 - signal_1)  # b
+    restrictions.append(signal_1)  # c
+    restrictions.append(signal_4 - signal_1)  # d
+    restrictions.append(None)  # e
+    restrictions.append(signal_1)  # f
+    restrictions.append(None)  # g
+
+    for arrangement in permutations(list(wires)):
+        ignore = False
+        for pos, restriction in enumerate(restrictions):
+            if restriction and arrangement[pos] not in restriction:
+                ignore = True
+                break
+        if ignore:
+            continue
+
+        m = {c: wires[i] for i, c in enumerate(arrangement)}
+        if map_valid(m):
+            return m
+
+
+def resolve_signal_map_fast(signals) -> dict:
+    """Given a set of signals output a mapping to the defaults
+    This is the original first attempt but I did not like the nested for loops
+    Turns out it is quicker that the more neater permutation method"""
     options = {w: set(wires) for w in wires}
 
     def map_valid(m):
@@ -103,7 +146,7 @@ def solve_part_b(data) -> int:
     """Solve part B"""
     values = []
     for signals, digits in data:
-        m = resolve_signal_map(signals)
+        m = resolve_signal_map_neat(signals)
         pl = 1000
         v = 0
         for en in digits:
