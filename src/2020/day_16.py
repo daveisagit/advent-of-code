2526c23992b7bded56cfa6dcd1e5099c8ec57449
Dave Budd <david.budd@ovo.com>
2024-01-26 12:27:57 +0000

    Day 16

---
+++ src/2020/day_16.py
@@ -0,0 +1,154 @@
+"""Advent of code 2020
+--- Day 16: Ticket Translation ---
+"""
+
+from common.aoc import file_to_list, aoc_part, get_filename
+from common.general import tok
+
+
+def parse_data(raw_data):
+    """Parse the input"""
+    rules = []
+    for i, line in enumerate(raw_data):
+        if not line:
+            break
+        arr = tok(line, ":")
+        rule_name = arr[0]
+        arr = tok(arr[1], "or")
+        rule = []
+        for r in arr:
+            ri = [int(x) for x in tok(r, "-")]
+            rule.append(ri)
+        rules.append(rule)
+
+    yt = [int(x) for x in raw_data[i + 2].split(",")]
+
+    nb = []
+    for i, line in enumerate(raw_data[i + 5 :]):
+        nb.append([int(x) for x in line.split(",")])
+
+    return rules, yt, nb
+
+
+def invalid_values(ticket, rules):
+    """Return a set of invalid values on this ticket"""
+    inv_val = set()
+    for fv in ticket:
+        complies = False
+        for r in rules:
+            if complies:
+                break
+            for opt in r:
+                if opt[0] <= fv <= opt[1]:
+                    complies = True
+                    break
+        if not complies:
+            inv_val.add(fv)
+    return inv_val
+
+
+@aoc_part
+def solve_part_a(data) -> int:
+    """Solve part A"""
+    rules, _, nearby = data
+    sr = 0
+    for ticket in nearby:
+        vals = invalid_values(ticket, rules)
+        if vals:
+            sr += sum(vals)
+    return sr
+
+
+def dump_matrix(rf):
+    """Visual"""
+    print()
+    for r in rf:
+        line = ["X" if v else " " for v in r]
+        print(" ".join(line))
+
+
+def apply_inference(rf):
+    """Look for and apply inferences"""
+    change_made = False
+    sz = len(rf)
+    inferences = []
+    for ri, fields in enumerate(rf):
+        possible_fields = [fi for fi, pos in enumerate(fields) if pos]
+        if len(possible_fields) == 1:
+            inferences.append((ri, possible_fields[0]))
+
+    for r, c in inferences:
+        for i in range(sz):
+            if i == r:
+                continue
+            if rf[i][c] is True:
+                change_made = True
+            rf[i][c] = False
+
+    return change_made
+
+
+@aoc_part
+def solve_part_b(data) -> int:
+    """Solve part B"""
+    rules, ours, nearby = data
+    valid_tickets = [ticket for ticket in nearby if not invalid_values(ticket, rules)]
+
+    # rule / field matrix
+    rf = []
+    c = len(rules)
+    for _ in range(c):
+        fp = list(False for _ in range(c))
+        rf.append(fp)
+
+    # initialize the matrix based on ticket data
+    for ri, rule in enumerate(rules):
+        for fi in range(c):
+            valid_on_all = True
+            for ticket in valid_tickets:
+                fv = ticket[fi]
+                valid_ticket = False
+                for opt in rule:
+                    if opt[0] <= fv <= opt[1]:
+                        valid_ticket = True
+                        break
+                if not valid_ticket:
+                    valid_on_all = False
+                    break
+            rf[ri][fi] = valid_on_all
+
+    # apply inferences
+    changes_made = True
+    while changes_made:
+        changes_made = apply_inference(rf)
+
+    # map rule to field
+    rule_to_field = [
+        ci for ri, fields in enumerate(rf) for ci, v in enumerate(fields) if v
+    ]
+
+    which_rules = 3
+    if len(rules) == 20:
+        which_rules = 6
+    ans = 1
+    for rule in range(which_rules):
+        fi = rule_to_field[rule]
+        v = ours[fi]
+        ans *= v
+
+    return ans
+
+
+EX_RAW_DATA = file_to_list(get_filename(__file__, "ex"))
+EX_DATA = parse_data(EX_RAW_DATA)
+
+
+MY_RAW_DATA = file_to_list(get_filename(__file__, "my"))
+MY_DATA = parse_data(MY_RAW_DATA)
+
+
+solve_part_a(EX_DATA)
+solve_part_a(MY_DATA)
+
+solve_part_b(EX_DATA)
+solve_part_b(MY_DATA)
