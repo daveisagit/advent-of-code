6515858d8414e676ee66b55a643ae55c30b833b0
Dave Budd <david.budd@ovo.com>
2024-04-01 09:27:56 +0100

    Day 12

---
+++ src/2017/day_12.py
@@ -0,0 +1,69 @@
+"""Advent of code 2017
+--- Day 12: Digital Plumber ---
+"""
+
+from collections import defaultdict, deque
+from common.aoc import file_to_list, aoc_part, get_filename
+from common.general import tok
+
+
+def parse_data(raw_data):
+    """Parse the input"""
+    gph = defaultdict(dict)
+    for line in raw_data:
+        arr = tok(line, "<->")
+        pid = int(arr[0])
+        com = [int(p) for p in tok(arr[1], ",")]
+        for p in com:
+            gph[pid][p] = 1
+            gph[p][pid] = 1
+    return gph
+
+
+def same_group(gph, p):
+    """Return a set of IDs in the same group"""
+    bfs = deque()
+    bfs.append(p)
+    seen = set()
+    while bfs:
+        p = bfs.popleft()
+        if p in seen:
+            continue
+        seen.add(p)
+        for q in gph[p]:
+            bfs.append(q)
+    return seen
+
+
+@aoc_part
+def solve_part_a(gph) -> int:
+    """Solve part A"""
+    sg = same_group(gph, 0)
+    return len(sg)
+
+
+@aoc_part
+def solve_part_b(gph) -> int:
+    """Solve part B"""
+    all_pgm = set(gph)
+    grp_cnt = 0
+    while all_pgm:
+        a = list(all_pgm)[0]
+        sg = same_group(gph, a)
+        grp_cnt += 1
+        all_pgm.difference_update(sg)
+
+    return grp_cnt
+
+
+EX_RAW_DATA = file_to_list(get_filename(__file__, "ex"))
+EX_DATA = parse_data(EX_RAW_DATA)
+
+MY_RAW_DATA = file_to_list(get_filename(__file__, "my"))
+MY_DATA = parse_data(MY_RAW_DATA)
+
+solve_part_a(EX_DATA)
+solve_part_a(MY_DATA)
+
+solve_part_b(EX_DATA)
+solve_part_b(MY_DATA)
