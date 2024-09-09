34a01b8cd29e3ae1bde40fadfcd50c8e1bb9f874
Dave Budd <david.budd@ovo.com>
2024-09-09 12:19:18 +0100

    Set theory

--- src/2021/day_22.py
+++ src/2021/day_22.py
@@ -2,10 +2,14 @@
 --- Day 22: Reactor Reboot ---
 """
 
+from bisect import bisect_left
+from collections import Counter, defaultdict
 from itertools import pairwise
 import math
 from common.aoc import file_to_list, aoc_part, get_filename
+from common.blocks import BlockResolver
 from common.general import tok
+from blocksets import Block
 
 
 def parse_data(raw_data):
@@ -201,6 +205,171 @@ def solve_part_b(data) -> int:
     return solve(data, trim=0)
 
 
+@aoc_part
+def solve_part_c(data) -> int:
+    """Solve part A"""
+
+    def cross_section_resolver(cross_section):
+        state = False
+        for _, stack_data in cross_section[::-1]:
+            op = stack_data[0]
+            if op is True:
+                state = not state
+                break
+            if op is False:
+                break
+
+        return state
+
+    br = BlockResolver(3, cross_section_resolver)
+    for op, ranges in data:
+        a = [0] * 3
+        b = [0] * 3
+        for d in range(3):
+            a[d] = max(ranges[d][0], -50)
+            b[d] = min(ranges[d][1], 50) + 1
+        block = (tuple(a), tuple(b))
+        stack_data = (op,)
+        entry = (block, stack_data)
+        if all(-50 <= r[0] <= 50 or -50 <= r[1] <= 50 for r in ranges):
+            br._operation_stack.append(entry)
+    br.resolve()
+    total = 0
+    for block, stack_data in br._operation_stack:
+        x_amt = block[1][0] - block[0][0]
+        y_amt = block[1][1] - block[0][1]
+        z_amt = block[1][2] - block[0][2]
+        total += x_amt * y_amt * z_amt
+
+    return total
+
+
+@aoc_part
+def solve_part_d(data) -> int:
+    """Solve part A"""
+
+    def cross_section_resolver(cross_section):
+        state = False
+        for _, stack_data in cross_section[::-1]:
+            op = stack_data[0]
+            if op is True:
+                state = not state
+                break
+            if op is False:
+                break
+
+        return state
+
+    br = BlockResolver(3, cross_section_resolver)
+    for op, ranges in data:
+        if any(any(abs(x) <= 50 for x in r) for r in ranges):
+            continue
+        a = [0] * 3
+        b = [0] * 3
+        for d in range(3):
+            a[d] = ranges[d][0]
+            b[d] = ranges[d][1] + 1
+        block = (tuple(a), tuple(b))
+        stack_data = (op,)
+        entry = (block, stack_data)
+        br._operation_stack.append(entry)
+    print(len(br._operation_stack))
+    br.resolve()
+    total = 0
+    for block, stack_data in br._operation_stack:
+        x_amt = block[1][0] - block[0][0]
+        y_amt = block[1][1] - block[0][1]
+        z_amt = block[1][2] - block[0][2]
+        total += x_amt * y_amt * z_amt
+
+    return total
+
+
+def get_markers(data):
+    """Hows the grid divided in terms of blocks"""
+    markers = []
+    ranges = [r for _, r in data]
+    for dim in range(3):
+        vals_1 = {x[dim][0] for x in ranges}
+        vals_2 = {x[dim][1] for x in ranges}
+        vals = vals_1 | vals_2
+        vals = sorted(vals)
+        markers.append(vals)
+    return markers
+
+
+@aoc_part
+def solve_part_e(data) -> int:
+    """Solve part A"""
+    new_data = []
+    for op, ranges in data:
+        (ax, bx), (ay, by), (az, bz) = ranges
+        if not any(any(abs(x) <= 50 for x in r) for r in ranges):
+            continue
+        new_row = op, ((ax, bx + 1), (ay, by + 1), (az, bz + 1))
+        new_data.append(new_row)
+    print(len(new_data))
+    markers = get_markers(new_data)
+    state = defaultdict(bool)
+    for op, ((ax, bx), (ay, by), (az, bz)) in new_data:
+
+        # axi = markers[0].index(ax)
+        # bxi = markers[0].index(bx)
+        # ayi = markers[1].index(ay)
+        # byi = markers[1].index(by)
+        # azi = markers[2].index(az)
+        # bzi = markers[2].index(bz)
+
+        axi = bisect_left(markers[0], ax)
+        bxi = bisect_left(markers[0], bx)
+        ayi = bisect_left(markers[1], ay)
+        byi = bisect_left(markers[1], by)
+        azi = bisect_left(markers[2], az)
+        bzi = bisect_left(markers[2], bz)
+
+        for xi in range(axi, bxi):
+            for yi in range(ayi, byi):
+                for zi in range(azi, bzi):
+                    blk = (xi, yi, zi)
+                    state[blk] = op
+
+    total = 1134088246458611
+    for (xi, yi, zi), ft in state.items():
+        if not ft:
+            continue
+        x_amt = markers[0][xi + 1] - markers[0][xi]
+        y_amt = markers[1][yi + 1] - markers[1][yi]
+        z_amt = markers[2][zi + 1] - markers[2][zi]
+        total += x_amt * y_amt * z_amt
+
+    return total
+
+
+@aoc_part
+def solve_part_f(data) -> int:
+    """Solve part A"""
+    new_data = []
+    for op, ranges in data:
+        (ax, bx), (ay, by), (az, bz) = ranges
+        new_row = op, (ax, ay, az), (bx + 1, by + 1, bz + 1)
+        new_data.append(new_row)
+
+    cubes = Counter()
+    for op, a, b in new_data:
+        block = Block(a, b)
+        update = Counter()
+        for c, s in cubes.items():
+            i = block & c
+            if i is None:
+                continue
+            update[i.norm] -= s
+        if op:
+            update[block.norm] += 1
+        cubes.update(update)
+
+    return sum(s * (Block.parse(b)).measure for b, s in cubes.items())
+
+
 EX_RAW_DATA = file_to_list(get_filename(__file__, "ex"))
 EX_DATA = parse_data(EX_RAW_DATA)
 
@@ -209,8 +378,13 @@ MY_RAW_DATA = file_to_list(get_filename(__file__, "my"))
 MY_DATA = parse_data(MY_RAW_DATA)
 
 
-solve_part_a(EX_DATA)
-solve_part_a(MY_DATA)
-
+# solve_part_a(EX_DATA)
+# solve_part_a(MY_DATA)
 solve_part_b(EX_DATA)
-solve_part_b(MY_DATA)
+# solve_part_b(MY_DATA) # 130s
+
+# solve_part_c(EX_DATA)
+# solve_part_d(MY_DATA)  # 310s
+# solve_part_e(MY_DATA)  # 200s
+
+solve_part_f(EX_DATA)  # 310s
