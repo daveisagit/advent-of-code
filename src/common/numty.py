ca147de27da5208389564df29b73ca5b1698708f
Dave Budd <david.budd@ovo.com>
2024-09-20 12:39:56 +0100

    primes and totient

--- src/common/numty.py
+++ src/common/numty.py
@@ -2,13 +2,14 @@
 
 from collections import Counter
 from functools import lru_cache
-from itertools import combinations, pairwise
-import math
+from itertools import combinations, islice, pairwise
+from math import sqrt, gcd
 from sys import getrecursionlimit, setrecursionlimit
 
 
 def prime_list(n):
-    """Return all the primes up to n"""
+    """Return all the primes up to n
+    PREFER list_of_primes()"""
     if n <= 2:
         return []
     sieve = [True] * (n + 1)
@@ -19,9 +20,77 @@ def prime_list(n):
     return [2] + [i for i in range(3, n, 2) if sieve[i]]
 
 
+# or if generating primes with no limit
+
+
+def generate_primes():
+    """Generate an infinite sequence of prime numbers."""
+    # Maps composites to primes witnessing their compositeness.
+    # This is memory efficient, as the sieve is not "run forward"
+    # indefinitely, but only as long as required by the current
+    # number being tested.
+    #
+    D = {}
+
+    # The running integer that's checked for primeness
+    q = 2
+
+    while True:
+        if q not in D:
+            # q is a new prime.
+            # Yield it and mark its first multiple that isn't
+            # already marked in previous iterations
+            #
+            yield q
+            D[q * q] = [q]
+        else:
+            # q is composite. D[q] is the list of primes that
+            # divide it. Since we've reached q, we no longer
+            # need it in the map, but we'll mark the next
+            # multiples of its witnesses to prepare for larger
+            # numbers
+            #
+            for p in D[q]:
+                D.setdefault(p + q, []).append(p)
+            del D[q]
+
+        q += 1
+
+
+# for x in generate_primes():
+#     print(x)
+#     if x > 100:
+#         break
+
+# l = list(islice(generate_primes(), 50))
+# print(l)
+
+
+def list_of_primes(n):
+    """Return all the primes <= n as a list"""
+    l = []
+    for p in generate_primes():
+        if p > n:
+            break
+        l.append(p)
+    return l
+
+
+# def phi_safe(n):
+#     """Euler's totient function
+#     The number positive integers up to n
+#     that are relatively prime to n"""
+#     amount = 0
+#     for k in range(1, n + 1):
+#         if gcd(n, k) == 1:
+#             amount += 1
+#     return amount
+
+
 def prime_factors(n):
-    """Return all the prime factors of n"""
-    primes = prime_list(int(math.sqrt(n)) + 1)
+    """Return all the prime factors of n
+    360: [2, 2, 2, 3, 3, 5]"""
+    primes = list_of_primes(int(sqrt(n)) + 1)
     factors = []
     for p in primes:
         while n % p == 0:
@@ -33,9 +102,21 @@ def prime_factors(n):
 
 
 def prime_factorized(n):
-    """Return all the prime factors of n"""
+    """Return all the prime factors of n
+    360: [(2, 3), (3, 2), (5, 1)]"""
     pfs = prime_factors(n)
     return list((p, a) for p, a in Counter(pfs).items())
+
+
+def phi(n):
+    """Euler's totient function
+    The number positive integers up to n
+    that are relatively prime to n"""
+    pfs = prime_factorized(n)
+    r = 1
+    for p, k in pfs:
+        r *= (p - 1) * p ** (k - 1)
+    return r
 
 
 def extended_euclid(a: int, b: int):
